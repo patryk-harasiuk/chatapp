7dc9c5b3567f0d9f41ab6c78e97bf82b5a25ec1f
Patryk Harasiuk <patryk.harasiuk@op.pl>
2021-05-06 13:20:30 +0200

    Getting last messages

--- server/services/getMessages.js
+++ server/services/getMessages.js
@@ -1,10 +1,14 @@
 const Room = require("../model/roomModel");
 
+// const getMessagesWithPopulate = (roomId) => {
+//   return Room.findById(roomId).populate({
+//     path: "messages",
+//     options: { perDocumentLimit: 25 },
+//   });
+// };
+
 const getMessagesWithPopulate = (roomId) => {
-  return Room.findById(roomId).populate({
-    path: "messages",
-    perDocumentLimit: 20,
-  });
+  return Room.findById(roomId).populate("messages").slice("messages", -25);
 };
 
 module.exports = getMessagesWithPopulate;
